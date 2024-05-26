b44e2a6b969ef5fa52d6063b26727706105554fc
s3897362 <80383166+s3897362@users.noreply.github.com>
2024-05-26 13:10:21 +1000

    Initial review controller and routes in place

---
+++ backend/src/routes/reviews.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const users = require("../controllers/user.review"); // Adjust the path as necessary
+const router = express.Router();
+
+
+
+
+module.exports = router;
