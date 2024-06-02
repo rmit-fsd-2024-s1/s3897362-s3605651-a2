6d333adab3879259f0af7457c67122c63110ddcb
Sagar Datta <sagar.datta@student.rmit.edu.au>
2024-06-02 15:06:24 +0800

    Display specials on home

--- admin-frontend/src/components/Home.js
+++ admin-frontend/src/components/Home.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_PRODUCTS = gql`
+  query GetAllProducts {
+    getAllProducts {
+      name
+      isSpecial
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const specialProducts = data.getAllProducts.filter(
+    (product) => product.isSpecial
+  );
+
   return (
-    <Box p={5} width="100%">
-      <Text fontSize="2xl" fontWeight="bold">
-        Soil Organics Dashboard
-      </Text>
-    </Box>
+    <div>
+      <h1>Special Products:</h1>
+      {specialProducts.map((product) => (
+        <p key={product.name}>{product.name}</p>
+      ))}
+    </div>
   );
 };
 
