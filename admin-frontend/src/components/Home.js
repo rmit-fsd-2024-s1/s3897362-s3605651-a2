9f3588961e7b654169aaa4cc59ed48f1b23a67b5
Sagar Datta <sagar.datta@student.rmit.edu.au>
2024-06-02 15:40:14 +0800

    feat: Add @nivo/pie library for displaying pie chart on home page

--- admin-frontend/src/components/Home.js
+++ admin-frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
+import { ResponsivePie } from "@nivo/pie";
 
 const GET_PRODUCTS = gql`
   query GetAllProducts {
@@ -19,6 +20,22 @@ const Home = () => {
   const specialProducts = data.getAllProducts.filter(
     (product) => product.isSpecial
   );
+  const notSpecialProducts = data.getAllProducts.filter(
+    (product) => !product.isSpecial
+  );
+
+  const chartData = [
+    {
+      id: "special",
+      label: "Special",
+      value: specialProducts.length,
+    },
+    {
+      id: "notSpecial",
+      label: "Not Special",
+      value: notSpecialProducts.length,
+    },
+  ];
 
   return (
     <div>
@@ -26,6 +43,18 @@ const Home = () => {
       {specialProducts.map((product) => (
         <p key={product.name}>{product.name}</p>
       ))}
+      <div style={{ height: 400 }}>
+        <ResponsivePie
+          data={chartData}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          colors={{ scheme: "nivo" }}
+          borderWidth={1}
+          borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+        />
+      </div>
     </div>
   );
 };
