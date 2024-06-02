cedaf103ac886815e2a0041e148b00dd231209dd
Sagar Datta <sagar.datta@student.rmit.edu.au>
2024-06-02 13:33:30 +0800

    loading when loading reviews

--- frontend/src/components/ReviewModal.js
+++ frontend/src/components/ReviewModal.js
@@ -28,6 +28,8 @@ const ReviewModal = ({ isOpen, onClose, productId, productName }) => {
   const [isEditingReview, setIsEditingReview] = useState(false);
   const [editingReview, setEditingReview] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleEditReview = (review) => {
     setEditingReview(review);
     setIsEditingReview(true);
@@ -43,6 +45,7 @@ const ReviewModal = ({ isOpen, onClose, productId, productName }) => {
   }, [isOpen, productId, toast]);
 
   const fetchReviews = async () => {
+    setIsLoading(true);
     // Clear the previous reviews
     setReviews([]);
 
@@ -58,6 +61,8 @@ const ReviewModal = ({ isOpen, onClose, productId, productName }) => {
         duration: 3000,
         isClosable: true,
       });
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -100,127 +105,132 @@ const ReviewModal = ({ isOpen, onClose, productId, productName }) => {
         <ModalHeader color="heading">{productName}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Box>
-            <Box textColor={"heading"}>
-              <Text fontSize="md" fontWeight="bold" mb={2}>
-                Average user rating:{" "}
-                <StarRatings
-                  rating={Number(averageRating) || 0}
-                  starRatedColor="gold"
-                  starEmptyColor="gray"
-                  starDimension="25px"
-                  starSpacing="2px"
-                  numberOfStars={5}
-                  name="rating"
-                />
-              </Text>
-            </Box>
-            <Text color="heading" fontSize="md" fontWeight="bold" mb={2}>
-              Individual Reviews:
-            </Text>
-            {reviews.map((review) => (
-              <Box
-                key={review.review_id}
-                shadow="lg"
-                borderWidth="1px"
-                borderRadius="lg"
-                position="relative"
-                bg="card"
-                textColor={"heading"}
-                height="100%"
-                display="flex"
-                flexDirection="column"
-                transition="all 0.2s"
-                _hover={{ transform: "scale(1.01)" }}
-                overflow="hidden"
-                borderColor={"beige"}
-                p={5}
-                mb={2}
-              >
-                <Box position="relative">
-                  <Flex direction="column" align="flex-start">
-                    <Flex
-                      fontWeight="bold"
-                      flexDirection="row"
-                      alignItems="center"
-                    >
-                      <Text mr={2}>User:</Text>
-                      <Text color="text"> {review.User.username}</Text>
-                    </Flex>
-                    <Text fontWeight="bold">
-                      Rating:{" "}
-                      <StarRatings
-                        rating={Number(review.rating) || 0}
-                        starRatedColor="gold"
-                        starEmptyColor="gray"
-                        starDimension="20px"
-                        starSpacing="2px"
-                        numberOfStars={5}
-                        name="rating"
-                      />
-                    </Text>
-                    <Flex
-                      fontWeight="bold"
-                      flexDirection="row"
-                      alignItems="center"
-                    >
-                      <Text mr={2}>Review:</Text>
-                      <Text color="text"> {review.review_text}</Text>
-                    </Flex>
-                  </Flex>
-                  {currentUserId && currentUserId === review.user_id && (
-                    <Box position="absolute" top={2} right={2}>
-                      <IconButton
-                        aria-label="Delete review"
-                        icon={<DeleteIcon />}
-                        colorScheme="red"
-                        onClick={() => handleDeleteReview(review.review_id)}
-                        mr={2}
-                        size="sm"
-                      />
-                      <IconButton
-                        aria-label="Edit review"
-                        icon={<EditIcon />}
-                        colorScheme="blue"
-                        onClick={() => handleEditReview(review)}
-                        size="sm"
-                      />
-                    </Box>
-                  )}
-                </Box>
+          {isLoading ? (
+            <Text>Loading reviews...</Text>
+          ) : (
+            <Box>
+              <Box textColor={"heading"}>
+                <Text fontSize="md" fontWeight="bold" mb={2}>
+                  Average user rating:{" "}
+                  <StarRatings
+                    rating={Number(averageRating) || 0}
+                    starRatedColor="gold"
+                    starEmptyColor="gray"
+                    starDimension="25px"
+                    starSpacing="2px"
+                    numberOfStars={5}
+                    name="rating"
+                  />
+                </Text>
               </Box>
-            ))}
-            {reviews.length === 0 && <Text>No reviews have been made.</Text>}
+              <Text color="heading" fontSize="md" fontWeight="bold" mb={2}>
+                Individual Reviews:
+              </Text>
 
-            <Button
-              colorScheme="blue"
-              onClick={() => setIsReviewFormOpen(true)}
-              mt={4}
-            >
-              Write a Review
-            </Button>
+              {reviews.map((review) => (
+                <Box
+                  key={review.review_id}
+                  shadow="lg"
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  position="relative"
+                  bg="card"
+                  textColor={"heading"}
+                  height="100%"
+                  display="flex"
+                  flexDirection="column"
+                  transition="all 0.2s"
+                  _hover={{ transform: "scale(1.01)" }}
+                  overflow="hidden"
+                  borderColor={"beige"}
+                  p={5}
+                  mb={2}
+                >
+                  <Box position="relative">
+                    <Flex direction="column" align="flex-start">
+                      <Flex
+                        fontWeight="bold"
+                        flexDirection="row"
+                        alignItems="center"
+                      >
+                        <Text mr={2}>User:</Text>
+                        <Text color="text"> {review.User.username}</Text>
+                      </Flex>
+                      <Text fontWeight="bold">
+                        Rating:{" "}
+                        <StarRatings
+                          rating={Number(review.rating) || 0}
+                          starRatedColor="gold"
+                          starEmptyColor="gray"
+                          starDimension="20px"
+                          starSpacing="2px"
+                          numberOfStars={5}
+                          name="rating"
+                        />
+                      </Text>
+                      <Flex
+                        fontWeight="bold"
+                        flexDirection="row"
+                        alignItems="center"
+                      >
+                        <Text mr={2}>Review:</Text>
+                        <Text color="text"> {review.review_text}</Text>
+                      </Flex>
+                    </Flex>
+                    {currentUserId && currentUserId === review.user_id && (
+                      <Box position="absolute" top={2} right={2}>
+                        <IconButton
+                          aria-label="Delete review"
+                          icon={<DeleteIcon />}
+                          colorScheme="red"
+                          onClick={() => handleDeleteReview(review.review_id)}
+                          mr={2}
+                          size="sm"
+                        />
+                        <IconButton
+                          aria-label="Edit review"
+                          icon={<EditIcon />}
+                          colorScheme="blue"
+                          onClick={() => handleEditReview(review)}
+                          size="sm"
+                        />
+                      </Box>
+                    )}
+                  </Box>
+                </Box>
+              ))}
+              {reviews.length === 0 && <Text>No reviews have been made.</Text>}
 
-            {isReviewFormOpen && (
-              <ReviewForm
-                isOpen={isReviewFormOpen}
-                onClose={() => setIsReviewFormOpen(false)}
-                productId={productId}
-                userId={currentUserId}
-                onSubmit={handleSubmitReview}
-              />
-            )}
-            {isEditingReview && (
-              <ReviewEdit
-                isOpen={true} // Make sure it's set to true when you want to render the modal
-                onClose={() => setIsEditingReview(false)} // Make sure you pass onClose function
-                review={editingReview}
-                onUpdate={() => {
-                  setIsEditingReview(false);
-                  fetchReviews(); // Update the reviews list after editing
-                }}
-              />
-            )}
-          </Box>
+              <Button
+                colorScheme="blue"
+                onClick={() => setIsReviewFormOpen(true)}
+                mt={4}
+              >
+                Write a Review
+              </Button>
+
+              {isReviewFormOpen && (
+                <ReviewForm
+                  isOpen={isReviewFormOpen}
+                  onClose={() => setIsReviewFormOpen(false)}
+                  productId={productId}
+                  userId={currentUserId}
+                  onSubmit={handleSubmitReview}
+                />
+              )}
+              {isEditingReview && (
+                <ReviewEdit
+                  isOpen={true} // Make sure it's set to true when you want to render the modal
+                  onClose={() => setIsEditingReview(false)} // Make sure you pass onClose function
+                  review={editingReview}
+                  onUpdate={() => {
+                    setIsEditingReview(false);
+                    fetchReviews(); // Update the reviews list after editing
+                  }}
+                />
+              )}
+            </Box>
+          )}
         </ModalBody>
       </ModalContent>
     </Modal>
